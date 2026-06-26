ae80e2d563a0a35a552c8cdbd2508f86f87bc0c0
dania <daniashaghouri@gmail.com>
2026-06-27 00:15:16 +0300

    console testi

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -28,6 +28,7 @@ router.post("/register", async (req, res) => {
 // ---------------- LOGIN ----------------
 router.post("/login", async (req, res) => {
   try {
+    console.log("LOGIN BODY:", req.body);
     const result = await login(req.body);
 
     res.status(200).json(result);
